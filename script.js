5436899052fe145144c945ecc510b699c1896a5a
Stephanie Lin <stephlinya@gmail.com>
2021-10-12 13:31:11 -0700

    updated button styles and modal format

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ closeModal.addEventListener("click", function(){
 })
 
 function displayQuotes(){
-    let quotes = ["Not everything has to be to-do list item. Your pace is okay. Your best is enough.", "There is no right time. Just time, and what you choose to do with it.", "It is okay to rest. It is okay to nap."];
+    let quotes = ["Not everything has to be to-do list item. Your pace is okay. Your best is enough.", "There is no right time. Just time, and what you choose to do with it.", "It is okay to rest. It is okay to nap.", "You are worthy."];
 
     let randomQuote = Math.floor(Math.random()*quotes.length);
     quoteDisplay.textContent = quotes[randomQuote];
