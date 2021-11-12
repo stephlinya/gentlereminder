c79c637f56eec32c2aa0b01bbdc673de652a5685
Stephanie Lin <stephlinya@gmail.com>
2021-11-11 16:50:49 -0800

    updated styling and added background image for letter

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const quoteDisplay = document.getElementById("quote-display");
 
-const openModal = document.getElementById("open-modal");
-const closeModal = document.getElementById("close-modal");
+const openModal = document.getElementById("open-letter");
+const closeModal = document.getElementById("close-letter");
 const overlay = document.getElementById("overlay");
 
 openModal.addEventListener("click", function(){
@@ -14,7 +14,14 @@ closeModal.addEventListener("click", function(){
 })
 
 function displayQuotes(){
-    let quotes = ["Not everything has to be to-do list item. Your pace is okay. Your best is enough.", "There is no right time. Just time, and what you choose to do with it.", "It is okay to rest. It is okay to nap.", "You are worthy."];
+    let quotes = [
+        "Not everything has to be to-do list item. Your pace is okay. Your best is enough.", 
+        "There is no right time. Just time, and what you choose to do with it.", 
+        "It is okay to rest. It is okay to nap.", 
+        "You are worthy.",
+        "Done is better than perfect.",
+
+    ];
 
     let randomQuote = Math.floor(Math.random()*quotes.length);
     quoteDisplay.textContent = quotes[randomQuote];
