6913f1eac9b19f0d8730e3678b7aebedbac4cc6b
Stephanie Lin <stephlinya@gmail.com>
2021-11-30 10:01:03 -0800

    changed to pixel styling, added animations

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const quoteDisplay = document.getElementById("quote-display");
-
+const sparkles= document.getElementById("sparkles");
 const openLetter = document.getElementById("open-letter");
+const openLink = document.getElementById("open");
 const closeLetter = document.getElementById("close-letter");
 const overlay = document.getElementById("overlay");
 
 openLetter.addEventListener("click", function(){
     overlay.style.display = "block";
+    openLetter.style.display = "none";
+    sparkles.style.display = "none";
+    displayQuotes();
+})
+
+openLink.addEventListener("click", function(){
+    overlay.style.display = "block";
+    openLetter.style.display = "none";
+    sparkles.style.display = "none";
     displayQuotes();
 })
 
 closeLetter.addEventListener("click", function(){
     overlay.style.display = "none";
+    openLetter.style.display = "block";
 })
 
 function displayQuotes(){
@@ -20,7 +31,7 @@ function displayQuotes(){
         "It is okay to rest. It is okay to nap.", 
         "You are worthy.",
         "Done is better than perfect.",
-        "It is a maranthon and not a sprint.",
+        "It is a marathon and not a sprint.",
         "The closest way to success is to try one more time.",
     ];
 
