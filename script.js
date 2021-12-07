9223770a0dcedb79c0aef2bfba688cab406f3522
Stephanie Lin <stephlinya@gmail.com>
2021-12-07 14:43:44 -0800

    finalized transition to pixel style, added new fonts, and adjusted animations

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 const quoteDisplay = document.getElementById("quote-display");
-const sparkles= document.getElementById("sparkles");
+const sparklesRight= document.getElementById("sparkles-r");
+const sparklesLeft= document.getElementById("sparkles-l");
 const openLetter = document.getElementById("open-letter");
 const openLink = document.getElementById("open");
 const closeLetter = document.getElementById("close-letter");
 const overlay = document.getElementById("overlay");
+const title = document.getElementById("title");
+const btnText = document.getElementById("btn-text");
 
 openLetter.addEventListener("click", function(){
     overlay.style.display = "block";
     openLetter.style.display = "none";
-    sparkles.style.display = "none";
+    title.style.display = "none";
+    btnText.style.display = "none";
+    sparklesRight.style.display = "none";
+    sparklesLeft.style.display = "none";
     displayQuotes();
 })
 
 openLink.addEventListener("click", function(){
     overlay.style.display = "block";
     openLetter.style.display = "none";
-    sparkles.style.display = "none";
+    title.style.display = "none";
+    btnText.style.display = "none";
+    sparklesRight.style.display = "none";
+    sparklesLeft.style.display = "none";
     displayQuotes();
 })
 
 closeLetter.addEventListener("click", function(){
     overlay.style.display = "none";
     openLetter.style.display = "block";
+    title.style.display = "block";
+    btnText.style.display = "block";
+    sparklesRight.style.display = "block";
 })
 
 function displayQuotes(){
@@ -32,6 +44,11 @@ function displayQuotes(){
         "You are worthy.",
         "Done is better than perfect.",
         "It is a marathon and not a sprint.",
+        "I am enough just as I am.",
+        "We are all growing at our own pace.",
+        "I deserve to take my time.",
+        "Every moment I have the opportunity to choose myself.",
+        "I don't always have to strive to be more than or better. I can just be myself.",
         "The closest way to success is to try one more time.",
     ];
 
